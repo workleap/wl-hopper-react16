5683aef4fdaba76b5422e5ed2a69979d1efd365e
Alexandre Asselin <alexandre.asselin@gsoft.com>
2024-04-16 16:38:15 -0400

    Update the scripts to support rich icons

---
+++ packages/icons-react16/src/RichIcon.tsx
@@ -0,0 +1,70 @@
+import React, { forwardRef, type ElementType, type RefAttributes, type SVGProps, type ComponentProps } from "react";
+
+import styles from "./RichIcon.module.css";
+
+export interface RichIconProps extends ComponentProps<"svg"> {
+    /**
+     * The visual style of the icon.
+     */
+    variant?: "option1" | "option2" | "option3" | "option4" | "option5" | "option6" | "option7" | "option8";
+    /**
+    * The size of the icon.
+    */
+    size?: "md" | "lg" | "xl";
+    /**
+     * The source of the icon with a size of 24px.
+     */
+    src24: ElementType<Omit<SVGProps<SVGSVGElement>, "ref"> & RefAttributes<SVGSVGElement>>;
+    /**
+     * The source of the icon with a size of 32px.
+     */
+    src32: ElementType<Omit<SVGProps<SVGSVGElement>, "ref"> & RefAttributes<SVGSVGElement>>;
+    /**
+     * The source of the icon with a size of 40px.
+     */
+    src40: ElementType<Omit<SVGProps<SVGSVGElement>, "ref"> & RefAttributes<SVGSVGElement>>;
+}
+
+export const RichIcon = forwardRef<SVGSVGElement, RichIconProps>((props, ref) => {
+    const {
+        size = "md",
+        variant = "option7",
+        src24,
+        src32,
+        src40,
+        style,
+        className,
+        "aria-label": ariaLabel,
+        "aria-hidden": ariaHidden,
+        ...rest
+    } = props;
+
+    const sizeMappings = {
+        md: src24,
+        lg: src32,
+        xl: src40
+    };
+
+    const As = sizeMappings[size];
+
+    const classNames = [
+        styles["hop-RichIcon"],
+        styles[`hop-RichIcon--${variant}`],
+        className
+    ].filter(x => x !== undefined).join(" ");
+
+    return (
+        <As
+            style={style}
+            {...rest}
+            ref={ref}
+            focusable="false"
+            role="img"
+            aria-label={ariaLabel}
+            aria-hidden={(ariaLabel ? (ariaHidden || undefined) : true)}
+            className={classNames}
+        />
+    );
+});
+
+RichIcon.displayName = "RichIcon";
--- packages/icons-react16/src/index.ts
+++ packages/icons-react16/src/index.ts
@@ -1,4 +1,7 @@
 export * from "./Icon.tsx";
-export * from "./create-icon.tsx";
+export * from "./createIcon.tsx";
 export * from "./generated-icon-components/index.ts";
 
+export * from "./RichIcon.tsx";
+export * from "./createRichIcon.tsx";
+export * from "./generated-rich-icon-components/index.ts";
